b169a2e8269ccc5c334f876c2912ca6272598f4f
Ayush <ayushsingh12march@gmail.com>
2020-03-06 23:49:52 +0530

    sun created using css

--- src/components/home/events/Events.js
+++ src/components/home/events/Events.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './Events.css'
-import sun from '../../../assets/sun.png'
-
 class Events extends Component {
 	render() {
 		return (
 			<div id='events'>
-				<img src={sun} id="sun" />
+				<div id="sun" />
 			</div>
 		)
 	}
