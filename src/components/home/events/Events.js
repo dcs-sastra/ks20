6e884daf44c332b956731e9786e2f7d77b46d929
Deepakh Srinivasan <deepakh.dlms@outlook.com>
2020-03-08 19:02:10 +0530

    Added event tiles

--- src/components/home/events/Events.js
+++ src/components/home/events/Events.js
@@ -1,12 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import EventCard from './EventCard'
 import './Events.css'
+import { Grid } from '@material-ui/core';
 
 class Events extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			events: [{
+				"eventName": "DSC",
+				"eventImage": "https://miro.medium.com/max/1185/1*a1Sy9bAOy6vZzILhvhSYjA.png",
+				"eventDescription": "DSC SASTRA"
+			},
+			{
+				"eventName": "DSC",
+				"eventImage": "https://miro.medium.com/max/1185/1*a1Sy9bAOy6vZzILhvhSYjA.png",
+				"eventDescription": "DSC SASTRA"
+			},
+			{
+				"eventName": "DSC",
+				"eventImage": "https://miro.medium.com/max/1185/1*a1Sy9bAOy6vZzILhvhSYjA.png",
+				"eventDescription": "DSC SASTRA"
+			}]
+		}
+	}
 	render() {
 		return (
-			<div id='events'>
+			<div id='events' class="container-fluid">
 				Details on Events and Proshows Coming Soon...
-			</div>	
+				<Grid justify="space-between" container direction="row" alignItems="center">
+					{
+						this.state.events.map((value) => {
+							return <Grid xs={11} sm={5} md={5} lg={3} item><EventCard event={value} class="eventCard" /></Grid>
+						})
+					}
+				</Grid>
+			</div>
 		)
 	}
 }
